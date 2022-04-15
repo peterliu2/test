2ebc014cb2dcc1b2c3b8a944c24f05c54a9fe857
peter_liu <peter.liu.work@gmail.com>
2022-04-15 15:21:36 +0800

    add hello

---
+++ helloworld.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int fun_1()
+{
+	int i = 12;
+	i++;
+	printf("fun_1\n");
+	return i;
+}
+
+
+int main (int argc, char* argv[])
+{
+	//int k;
+	printf("hello world!\n");
+	//k = fun_1();
+	return 0;
+}
